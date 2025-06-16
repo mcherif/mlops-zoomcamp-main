f4bcc1c96b13f305004c809f20ead716e5039b76
Mohamed Cherif <1292828+mcherif@users.noreply.github.com>
2025-06-16 17:54:57 +0000

    refactoring + adding prefect

--- cohorts/2025/04-deployment/homework/starter.py
+++ cohorts/2025/04-deployment/homework/starter.py
@@ -5,15 +5,16 @@ import pickle
 import pandas as pd
 import numpy as np
 import argparse
-import boto3  # <-- Add this import
+import boto3
 import os
+from prefect import task, flow
+from typing import Optional
 
-def upload_to_s3(local_file, bucket, s3_file):
-    s3 = boto3.client('s3')
-    s3.upload_file(local_file, bucket, s3_file)
-    print(f"Uploaded {local_file} to s3://{bucket}/{s3_file}")
+# Define the categorical features
+CATEGORICAL = ['PULocationID', 'DOLocationID']
 
 # Function to read and preprocess the data
+@task(log_prints=True, retries=2, retry_delay_seconds=5)
 def read_data(filename):
     df = pd.read_parquet(filename)
 
@@ -21,21 +22,37 @@ def read_data(filename):
     df['duration'] = df.duration.dt.total_seconds() / 60
 
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
-    # Define the categorical features
-    categorical = ['PULocationID', 'DOLocationID']
-
-    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    df[CATEGORICAL] = df[CATEGORICAL].fillna(-1).astype('int').astype('str')
 
     return df
 
+@task(log_prints=True)
 def load_model(model_file='model.bin'):
     # Load the pre-trained model and DictVectorizer
     with open(model_file, 'rb') as f_in:
         dv, model = pickle.load(f_in)
     return dv, model
 
+@task(log_prints=True, retries=2)
+def upload_to_s3(local_file, bucket, s3_file):
+    s3 = boto3.client('s3')
+    s3.upload_file(local_file, bucket, s3_file)
+    print(f"Uploaded {local_file} to s3://{bucket}/{s3_file}")
 
-def run(year, month, model_file='model.bin', bucket=None):
+@task(log_prints=True)
+def make_predictions(df, dv, model, year, month):
+    dicts = df[CATEGORICAL].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
+    print(f"Standard deviation of y_pred: {np.std(y_pred)}")
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+    df_result = df[['ride_id']].copy()
+    df_result['prediction'] = y_pred
+    return df_result, y_pred
+
+@flow(name="Taxi Duration Prediction Flow")
+def main_flow(year: int, month: int, model_file: str = 'model.bin', bucket: Optional[str] = None):
     # Load the model and DictVectorizer
     dv, model = load_model(model_file)
 
@@ -47,23 +64,8 @@ def run(year, month, model_file='model.bin', bucket=None):
 
     # Read and preprocess the data
     df = read_data(input_file)
-    # TODO: improve by removing this (Redefine the categorical features)
-    categorical = ['PULocationID', 'DOLocationID']
     
-    dicts = df[categorical].to_dict(orient='records')
-    X_val = dv.transform(dicts)
-    y_pred = model.predict(X_val)
-
-    # Calculate and print the standard deviation of the predictions
-    y_pred_std = np.std(y_pred)
-    print(f"Standard deviation of y_pred: {y_pred_std}")
-
-    # Create a ride_id column in the format 'YYYY/MM_ride_index'
-    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index  .astype('str')
-
-
-    df_result = df[['ride_id']].copy()
-    df_result['prediction'] = y_pred
+    df_result, y_pred = make_predictions(df, dv, model, year, month)
 
     output_file = f'yellow_tripdata_{year:04d}-{month:02d}_predicted.parquet'
     df_result.to_parquet(output_file, engine='pyarrow', compression=None, index=False)
@@ -78,17 +80,14 @@ def run(year, month, model_file='model.bin', bucket=None):
     # return the mean predicted duration
     mean_pred = y_pred.mean()
     print(f"Mean predicted duration: {mean_pred}")
-    return str(mean_pred)
-
+    return mean_pred
 
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Run a pre-trained model to predict taxi trip duration for a given year and month.')
-    print("Example usage: python starter.py --year 2023 --month 3  --bucket my-bucket")
+    print("Example usage: python starter.py --year 2023 --month 3  --bucket my-s3-bucket")
     parser.add_argument('--year', type=int, required=True, help='Year of the data to train on')
     parser.add_argument('--month', type=int, required=True, help='Month of the data to train on')
     parser.add_argument('--bucket', type=str, required=False, help='S3 bucket to upload the result')
     args = parser.parse_args()
-    
-    # return the mean predicted duration and upload output to s3
-    mean_pred = run(year=args.year, month=args.month, bucket=args.bucket)
+    main_flow(year=args.year, month=args.month, bucket=args.bucket)
